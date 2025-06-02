516f20ad8b483ccf8f3cb2de160ec20e2ee19966
Haris Deralas <harisderalas@gmail.com>
2025-06-02 12:32:20 +0300

    Added visualization_utils class that also includes Sota (GMAN) predictions - for now it's empty, to be updated

---
+++ visualization_utils_with_sota_grouped_legend.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+import plotly.graph_objects as go
+from typing import Optional, Callable, List, Union, Dict, Tuple
+from traffic_flow_package_src.helper_utils import load_adjacency_dicts
+from traffic_flow_package_src.post_processing import PredictionCorrectionPerSensor
+from traffic_flow_package_src.post_processing import UpDownDict
+import warnings
+import logging
+from plotly.subplots import make_subplots
+import os
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# (The full content of the updated script would be here. Since the environment was reset,
+# we simulate the re-creation for download.)
