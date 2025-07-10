31dda27cb19a302944abe42e77146bc6ed0f8684
Haris Deralas <harisderalas@gmail.com>
2025-07-10 11:41:24 +0300

    Added new class BaseFeatureTranformer, using abstract classes from sklearn, in order tolater convert feature classes in a way that can be used in production (e.g. both fit and transform methods, so that all new inputs can be transformed, after the initial fitting used from training data

--- traffic_flow/features/base.py
+++ traffic_flow/features/base.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
+from gc import disable
 import pandas as pd
 from     typing import Iterable
+from sklearn.base import BaseEstimator, TransformerMixin
 from ..utils.helper_utils import LoggingMixin 
 
 class SensorEncodingStrategy(ABC, LoggingMixin):
@@ -9,7 +11,7 @@ class SensorEncodingStrategy(ABC, LoggingMixin):
     """
 
     def __init__(self, sensor_col: str = "sensor_id", new_sensor_col: str = "sensor_uid", disable_logs: bool = False):
-        super().__init__(disable_logs)
+        super().__init__(disable_logs=disable_logs)
         self.sensor_col = sensor_col
         self.new_sensor_col = new_sensor_col
 
@@ -36,7 +38,7 @@ class FeatureTransformer(ABC, LoggingMixin):
     """
 
     def __init__(self, disable_logs: bool = False):
-        super().__init__(disable_logs)
+        super().__init__(disable_logs=disable_logs)
 
     @abstractmethod
     def transform(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -58,6 +60,31 @@ class BaseAggregator(ABC):
     @abstractmethod
     def aggregate(self, values: Iterable[float]) -> float:
         """Return a single statistic from `values` (NaNs ignored)."""
+        
+        
+
+class BaseFeatureTransformer(BaseEstimator, TransformerMixin, LoggingMixin):
+    """
+    Common utilities & logging for all custom transformers.
+    Uses sklearn's BaseEstimator and TransformerMixin to make it a sklearn transformer.
+    This allows to use the feature classes in a production pipeline (which transforms any input data to ML-ready data)
+    """
+
+    def __init__(self, disable_logs: bool = False):
+        super().__init__(disable_logs=disable_logs)
+    
+
+    # sklearn calls fit once, then transform many times
+    def fit(self, X, y=None):
+        return self  # most simple transformers don’t need to learn anything
+
+    def transform(self, X):
+        raise NotImplementedError("Every subclass must implement transform()")
+
+    # Optional: keep a small helper
+    def _log(self, msg: str):
+        if not self.disable_logs:
+            print(f"[{self.__class__.__name__}] {msg}")
     
     
     
